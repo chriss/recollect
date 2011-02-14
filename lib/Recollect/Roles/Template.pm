chriss/recollect

commit 1f93effa9e7b039a96fdd1305f6e6a52afcecbc2

make these 2 roles work as class methods

lib/Recollect/Roles/Template.pm
@@ -1,20 +1,17 @@
 package Recollect::Roles::Template;
 use Moose::Role;
 use Template;
-use Recollect::Util qw/base_path/;
+use namespace::clean -except => 'meta';
 
 requires 'log';
+requires 'base_path';
 
-has 'tt2' => (is => 'ro', isa => 'Object', lazy_build => 1);
+our $TT2;
+sub tt2 { $TT2 ||= shift->_build_tt2 }
 
 sub _build_tt2 {
-    my $self = shift;
-    my $templ_path = base_path() . '/template';
-    unless (-d $templ_path) {
-        $self->log("Unknown template path: $templ_path from "
-                . $self->request->path);
-        return $self->not_found();
-    }
+    my $class = shift;
+    my $templ_path = $class->base_path() . '/template';
     return Template->new(
         { INCLUDE_PATH => $templ_path },
     );

lib/Recollect/Roles/Email.pm
@@ -10,7 +10,8 @@ requires 'config';
 requires 'base_url';
 requires 'tt2';
 
-has '_mailer' => (is => 'ro', isa => 'Object', lazy_build => 1);
+my $MAILER;
+sub _mailer { $MAILER ||= shift->_build__mailer }
 
 sub send_email {
     my $self = shift;
@@ -24,7 +25,8 @@ sub send_email {
     my $template = "email/$args{template}";
     $args{template_args}{base} = $self->base_url;
     my $body;
-    $self->tt2->process($template, $args{template_args}, \$body) ;
+    $self->tt2->process($template, $args{template_args}, \$body);
+    die "Error rendering $template: " . $self->tt2->error unless defined $body;
 
     my %headers = (
         From => $args{from} || 'Recollect <noreply@recollect.net>',
@@ -48,6 +50,7 @@ sub _build__mailer {
     my $self = shift;
 
     if ($ENV{RECOLLECT_EMAIL}) {
+        no warnings 'redefine';
         @Email::Send::IO::IO = ($ENV{RECOLLECT_EMAIL});
         return Email::Send->new({
             mailer => 'IO',

commit 160a71ef8f101b1b5612c7c80baadf8119a5ef0a

Allow the app to start again (1f93eff broke everything)

lib/Recollect/Roles/Template.pm
@@ -1,10 +1,15 @@
 package Recollect::Roles::Template;
 use Moose::Role;
 use Template;
+use Recollect::Util;
 use namespace::clean -except => 'meta';
 
 requires 'log';
-requires 'base_path';
+
+sub base_path { 
+    my $class = shift;
+    Recollect::Util::base_path(@_);
+}
 
 our $TT2;
 sub tt2 { $TT2 ||= shift->_build_tt2 }