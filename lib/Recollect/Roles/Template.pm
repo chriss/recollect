d91c32a391db2869470d61816dabc89b69951f17
Luke Closs <gravatar@5thplane.com>
2011-02-14 09:42:05 -0800

    fix a compile warnings

--- lib/Recollect/Roles/Template.pm
+++ lib/Recollect/Roles/Template.pm
@@ -1,22 +1,17 @@
 package Recollect::Roles::Template;
 use Moose::Role;
 use Template;
-use Recollect::Util;
+use Recollect::Util qw/base_path/;
 use namespace::clean -except => 'meta';
 
 requires 'log';
-
-sub base_path { 
-    my $class = shift;
-    Recollect::Util::base_path(@_);
-}
 
 our $TT2;
 sub tt2 { $TT2 ||= shift->_build_tt2 }
 
 sub _build_tt2 {
     my $class = shift;
-    my $templ_path = $class->base_path() . '/template';
+    my $templ_path = base_path() . '/template';
     return Template->new(
         { INCLUDE_PATH => $templ_path },
     );
