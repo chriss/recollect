8072fe75ceecd1ebb0707e31bbd46bd4fe8424a2
Luke Closs <gravatar@5thplane.com>
2011-02-22 01:24:01 -0800

    and the lord said, let there be data for toronto

--- etc/sql/areas.sql
+++ etc/sql/areas.sql
@@ -1,3 +1,4 @@
 -- This file lists the SQL to create areas
 
 INSERT INTO areas VALUES (nextval('area_seq'), 'Edmonton', '36.980056,-85.612191');
+INSERT INTO areas VALUES (nextval('area_seq'), 'Toronto', '43.565467,-79.361115');
