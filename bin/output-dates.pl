86adb8c01da69aaa700e559c5358da7be2fc247d
Kevin Jones <kvn.jns@gmail.com>
2011-02-24 00:35:33 -0800

    Add a script to make csv generation way easier

---
+++ bin/output-dates.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Term::ReadKey;
+use DateTime;
+
+my $zone = shift || die "USAGE: $0 <zone>, then use VI hjl keys!";
+
+my $date = DateTime->today;
+ReadMode('cbreak');
+
+while (1) {
+    warn $date->ymd . "\n";
+    my $char = ReadKey( 0 );
+
+    if ($char eq 'j') { # down
+        print $zone . ',' . $date->ymd . "\n";
+        $date->add(weeks => 1);
+    }
+    elsif ($char eq 'h') {
+        $date->subtract(days => 1);
+    }
+    elsif ($char eq 'l') {
+        $date->add(days => 1);
+    }
+    else {
+        warn "Unknown key!\n";
+    }
+}
