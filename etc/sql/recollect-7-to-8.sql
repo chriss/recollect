c4fb13a5f0c9003488987eacacce2ce5c8fef375
Luke Closs <gravatar@5thplane.com>
2011-03-23 22:25:28 -0700

    fix sql migration, sorry kevinj

--- etc/sql/recollect-7-to-8.sql
+++ etc/sql/recollect-7-to-8.sql
@@ -2,7 +2,7 @@ BEGIN;
 
 ALTER TABLE ical_users ADD COLUMN created_at timestamptz;
 UPDATE ical_users SET created_at = last_get;
-ALTER TABLE ical_users ADD CONSTRAINT SET NOT NULL;
+ALTER TABLE ical_users ADD CONSTRAINT created_at SET NOT NULL;
 
 DELETE FROM recollect_schema;
 INSERT INTO recollect_schema (current_version) VALUES (8);
