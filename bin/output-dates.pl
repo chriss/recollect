e36552262012ef67cfb148fc03029e2219b13107
Luke Closs <luke@recollect.net>
2011-04-18 20:54:21 -0700

    output tron, improve the input capability

--- bin/output-dates.pl
+++ bin/output-dates.pl
@@ -3,21 +3,45 @@ use strict;
 use warnings;
 use Term::ReadKey;
 use DateTime;
+use DateTime::Format::Pg;
+use Getopt::Long;
+use JSON;
+use autodie;
+
+my %opts = (
+    flags => 'XXX',
+    utc => '-08',
+);
+GetOptions( \%opts,
+    'start=s',
+    'flags=s',
+    'utc=s',
+    'output=s',
+);
 
 my $zone = shift || die "USAGE: $0 <zone>, then use VI hjl keys!";
 
 my $date = DateTime->today;
+if (my $s = $opts{start}) {
+    $date = DateTime::Format::Pg->parse_datetime($s);
+}
+
 ReadMode('cbreak');
 
 $| = 1;
 
+my @pickups;
 while (1) {
-    warn $date->ymd . "\n";
+    print $date->ymd, " - $opts{flags} ?\n";
     my $char = ReadKey( 0 );
 
     if ($char eq 'j') { # down
-        print $zone . ',' . $date->ymd . "\n";
-        warn "\t" . $zone . ',' . $date->ymd . "\n";
+        print "Adding " . $date->ymd . " $opts{flags}\n";
+        push @pickups, {
+            zone => $zone,
+            date => $date->ymd . " 07:00:00$opts{utc}",
+            flags => $opts{flags},
+        };
         $date->add(weeks => 1);
     }
     elsif ($char eq 'h') {
@@ -26,7 +50,28 @@ while (1) {
     elsif ($char eq 'l') {
         $date->add(days => 1);
     }
-    else {
-        warn "Unknown key!\n";
+    elsif ($char eq 'q') {
+        last;
     }
+    else {
+        print <<EOT;
+Help:
+  j - add one week
+  h - subtract one day
+  l - add one day
+  q - quit
+EOT
+        next;
+    }
+
 }
+
+Write_the_tron: {
+    my $file = $opts{output} || "$zone.tron";
+    print "Writing to $file\n";
+    open my $fh, ">", $file;
+    print $fh encode_json({ tron_version => 1, pickups => \@pickups });
+    close $fh;
+}
+
+ReadMode(0);
